c3b2ac39b510bc26ab0288a26f61d38c904f70c4
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-09-26 17:26:12 -0700

    Can add up to five items to grocery list

--- main.cpp
+++ main.cpp
@@ -16,6 +16,15 @@ int main()
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+  if(input=='a'||input=='A'){
+      if(numItems==5){
+    cout<<"ERROR: You'll need a bigger list!";
+}
+else if(numItems<5){ cout<<"What is the item?"<<endl;
+  cin>>list[numItems];
+  numItems++;
+}  
+}
 } while(input!='Q');
   return 0;
 }
