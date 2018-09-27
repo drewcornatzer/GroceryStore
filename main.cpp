c6ee4f6cefc4c059c57b824ca64fd7668d4f6ab9
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-09-26 17:04:29 -0700

    Do-while loop runs until Q is entered

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// Author: Drew Cornatzer
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +8,14 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+  do
+{
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-
+} while(input!='Q');
   return 0;
 }
