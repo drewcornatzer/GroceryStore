a29d82d629debae1f3b4c0bbcd3efa6173b246ee
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-10-01 17:57:57 -0700

    Prints grocery list of 5 items

--- main.cpp
+++ main.cpp
@@ -5,24 +5,37 @@ using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
-do{
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-  if(numItems==5&&(input=='a'||input=='A')){
-    cout<<"You'll need a bigger list!"<<endl;
-}
-
-  else if(input=='a'||input=='A'){
-  cout<<"What is the item?"<<endl;
-  cin>>list[numItems];
-  numItems++;}
-}while(input!='Q');
-  return 0;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        string item;
+        do{
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+                if(input=='a'||input=='A')
+                {
+                        cout<<"What is the item?"<<endl;
+                        cin>>item;
+                if(numItems<=4)
+                {
+                       list[numItems]=item;
+                       numItems++;
+                }
+                else if(numItems==5)
+                {
+                        cout<<"You'll need a bigger list!"<<endl;
+                }
+                }
+        }
+        while(input!='Q'&& input!='q');
+        for(int index=0;index<5;index++)
+        {
+                cout<<index+1;
+                cout<<"  "<<list[index]<<endl;
+        }
+        return 0;
 }
