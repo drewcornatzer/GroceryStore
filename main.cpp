0c16d93d597773c22791c4353f8ed8d858bd4d02
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-10-10 16:26:20 -0700

    Edited Grocery List with delete function

--- main.cpp
+++ main.cpp
@@ -10,18 +10,38 @@ int main()
         int numItems = 0;
         char input;
         string item;
-        do{
+        string deleteditem;
+        do{     
+                if(list.size()==0)
+                {
                 cout<<"\n==GROCERY LIST MANAGER==";
                 cout<<"\nEnter your choice: ";
                 cout<<"\n (A)dd an item";
                 cout<<"\n (Q)uit";
                 cout<<"\nYour choice (A/Q): ";
                 cin>>input;
+                }   
+                if(list.size()>0)
+                {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (D)elete last item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+                }
                 if(input=='a'||input=='A')
                 {
                         cout<<"What is the item?"<<endl;
                         cin>>item;
                         list.push_back(item);
+                }
+                if(input=='d'||input=='D'&&list.size()>0)
+                {
+                deleteditem=list.back();
+                list.pop_back();
+                cout<<deleteditem<<" was deleted from the list. \n";
                 }
         }
         while(input!='Q'&& input!='q');
